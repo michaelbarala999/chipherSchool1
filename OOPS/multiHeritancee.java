f388201021bd2cd75111fe565ff4e86c998b547a
michaelbarala999 <michaelbarala999@gmail.com>
2024-01-31 18:41:19 +0530

    oops

---
+++ OOPS/multiHeritancee.java
@@ -0,0 +1,42 @@
+
+/* package whatever //do not write package name here */
+
+import java.io.*;
+
+class A {
+    int age;
+
+    private void funPrivate() {
+        System.out.println("i am private function");
+    }
+
+    public void fun() {
+        System.out.println("i am from A class");
+    }
+}
+
+class B extends A {
+    public void fun2() {
+        System.out.println("i am from B class");
+    }
+}
+
+class C extends A {
+    public void funC() {
+        System.out.println("i am from C class");
+    }
+}
+
+class multiHeritancee {
+    public static void main(String[] args) {
+        A obj1 = new A();
+        obj1.fun();
+        B obj2 = new B();
+        obj2.fun2();
+        obj2.fun();
+        C obj3 = new C();
+        obj3.fun();
+        obj3.fun2();
+        obj3.funC();
+    }
+}
---
+++ comandLine.java
@@ -0,0 +1,9 @@
+import String.string;
+
+public class comandLine {
+    public static void main(String[] myString) {
+        for (int i = 0; i < myString.length; i++) {
+            System.out.println(myString[i]);
+        }
+    }
+}
